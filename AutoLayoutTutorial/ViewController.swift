285ed4cda6cc90c27fec8751e686ef93e00070f3
Stephen Dowless <stephendowless@homesnaminsmbp2.fios-router.home>
2019-04-02 20:46:47 -0400

    Initial Commit

---
+++ AutoLayoutTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutTutorial
+//
+//  Created by Stephen Dowless on 4/2/19.
+//  Copyright © 2019 Stephan Dowless. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
